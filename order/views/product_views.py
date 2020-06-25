7ccab7ea9f60522a2949a8c9cdcf1b3b28fada0a
klement97 <44920963+klement97@users.noreply.github.com>
2020-06-25 22:12:39 +0200

    fix: add serial to select related

--- order/views/product_views.py
+++ order/views/product_views.py
@@ -6,6 +6,7 @@ from order.serializers.product_serializers import ProductSerializer
 
 class ProductRetrieveAPIView(RetrieveAPIView):
     queryset = Product.objects \
-        .select_related('category', 'inner_leather', 'outer_leather') \
+        .select_related('category', 'inner_leather', 'outer_leather',
+                        'inner_leather__serial', 'outer_leather__serial') \
         .filter(deleted=False)
     serializer_class = ProductSerializer
