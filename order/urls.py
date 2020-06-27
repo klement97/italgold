0c4feff26239fe2cf04a9cc4f41a9ae2a6857d7a
klement97 <44920963+klement97@users.noreply.github.com>
2020-06-27 14:43:32 +0200

    feat: add product list api view

--- order/urls.py
+++ order/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from order.views.product_views import ProductRetrieveAPIView
+from order.views.product_views import ProductListAPIView, ProductRetrieveAPIView
 
 urlpatterns = [
-    path('product/<int:pk>/', ProductRetrieveAPIView.as_view(), name='product')
+    path('product/<int:pk>/', ProductRetrieveAPIView.as_view(), name='product'),
+    path('product/', ProductListAPIView.as_view(), name='product')
     ]
--- order/views/product_views.py
+++ order/views/product_views.py
@@ -1,5 +1,6 @@
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
+from order.filters import ProductFilter
 from order.models import Product
 from order.serializers.product_serializers import ProductSerializer
 
@@ -10,3 +11,12 @@ class ProductRetrieveAPIView(RetrieveAPIView):
                         'inner_leather__serial', 'outer_leather__serial') \
         .filter(deleted=False)
     serializer_class = ProductSerializer
+
+
+class ProductListAPIView(ListAPIView):
+    queryset = Product.objects \
+        .select_related('category', 'inner_leather', 'outer_leather',
+                        'inner_leather__serial', 'outer_leather__serial') \
+        .filter(deleted=False)
+    serializer_class = ProductSerializer
+    filterset_class = ProductFilter
