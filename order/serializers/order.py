3b8efbf6359e6158e2d5be7d204bf0d67d5a0d82
Klement Omeri <klementomeri97@gmail.com>
2020-09-23 22:02:41 +0200

    refactor

--- order/serializers/order.py
+++ order/serializers/order.py
@@ -19,18 +19,14 @@ class OrderSerializer(ModelSerializer):
 
     @atomic()
     def create(self, validated_data):
-        inner_leather_id = self.initial_data.get('inner_leather')
-        outer_leather_id = self.initial_data.get('outer_leather')
         order_units = validated_data.pop('order_units')
 
         order = Order.objects.create(**validated_data)
 
-        units = [OrderUnit(**{**unit,
-                              'order': order,
-                              'inner_leather_id': inner_leather_id,
-                              'outer_leather_id': outer_leather_id,
-                              'price': unit['product'].price
-                              })
+        units = [OrderUnit(**{**unit}, order=order, price=unit['product'].price,
+                           inner_leather_id=self.initial_data.get('inner_leather'),
+                           outer_leather_id=self.initial_data.get('outer_leather'),
+                           )
                  for unit in order_units]
 
         OrderUnit.objects.bulk_create(units)
