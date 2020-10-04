14e6439d7a9bb623c8a129159b6a34e9ab11abe7
Klement Omeri <klementomeri97@gmail.com>
2020-10-04 02:32:31 +0200

    remove cached files from git index

--- Procfile
+++
@@ -1,2 +0,0 @@
-release: python manage.py migrate
-web: gunicorn kral_kutu_backend.wsgi
--- order/views/order.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.generics import CreateAPIView
-
-from order.serializers.order import OrderSerializer
-
-
-class OrderCreateAPIView(CreateAPIView):
-    serializer_class = OrderSerializer
