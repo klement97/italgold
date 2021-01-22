925c6ae2bc7e46a1128fb66043b788f95a0ac2b5
Klement Omeri <klementomeri97@gmail.com>
2021-01-22 23:33:27 +0100

    chore: move filesystem settings to production_settings.py

--- kral_kutu_backend/production_settings.py
+++ kral_kutu_backend/production_settings.py
@@ -19,3 +19,13 @@ def settings(config):
             # django.contrib.auth) you may enable sending PII data.
             send_default_pii=True
             )
+
+    # Filesystem settings
+    config['DEFAULT_FILE_STORAGE'] = os.getenv('DEFAULT_FILE_STORAGE')
+    config['AWS_ACCESS_KEY_ID'] = os.getenv('AWS_ACCESS_KEY_ID')
+    config['AWS_SECRET_ACCESS_KEY'] = os.getenv('AWS_SECRET_ACCESS_KEY')
+    config['AWS_STORAGE_BUCKET_NAME'] = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    config['AWS_DEFAULT_ACL'] = None
+    config['AWS_QUERYSTRING_AUTH'] = False
+    config['AWS_QUERYSTRING_EXPIRE'] = 3600  # seconds
+    config['AWS_S3_FILE_OVERWRITE'] = False
--- kral_kutu_backend/settings.py
+++ kral_kutu_backend/settings.py
@@ -27,7 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
+SECRET_KEY = 'foo'
 
 ALLOWED_HOSTS = ['*']
 
@@ -162,7 +164,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles/')
 MEDIA_URL = '/mediafiles/'
 
-DEFAULT_FROM_EMAIL = 'no-reply@italgold.com'
+DEFAULT_FROM_EMAIL = 'italgold.dev@gmail.com'
 ADMINS = ['klementomeri97@gmail.com']
 MANAGERS = ['klementomeri97@gmail.com']
 
