1ceb9433daf9c760e3ee7ca62bc79c6a9eaac6c0
Klement Omeri <klementomeri97@gmail.com>
2021-01-30 16:48:41 +0100

    refactor: scripts

--- order/scripts.py
+++ order/scripts.py
@@ -5,21 +5,22 @@ from order.models import Product
 
 def get_product_from_dict(product_dict: dict) -> Product:
     return Product(
-            properties={
-                'code': product_dict['code'].upper(),
-                'width': product_dict['width'],
-                'length': product_dict['length'],
-                'height': product_dict['height'],
-                },
-            price=product_dict['price'],
-            category_id=product_dict['category'],
-            image=f'products/{product_dict["image"]}.jpg'
-            )
+        properties={
+            'code': product_dict['code'].upper(),
+            'width': product_dict['width'],
+            'length': product_dict['length'],
+            'height': product_dict['height'],
+            },
+        price=product_dict['price'],
+        category_id=product_dict['category'],
+        image=f'products/{product_dict["image"]}.jpg'
+        )
 
 
 def load_products_csv():
     with open('./csv_files/products.csv') as f:
-        field_names = ['code', 'width', 'length', 'height', 'price', 'category', 'image']
+        field_names = ['code', 'codes', 'width', 'length', 'height', 'price', 'prices', 'category',
+                       'image']
         file = csv.DictReader(f, fieldnames=field_names)
 
         new_products = []
