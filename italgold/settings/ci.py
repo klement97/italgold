edc491258d36ce1e50c50416435cba8511de4e20
Klement Omeri <klementomeri97@gmail.com>
2021-02-26 21:19:05 +0100

    ci: setup postgres db in github actions

--- italgold/settings/ci.py
+++ italgold/settings/ci.py
@@ -3,3 +3,14 @@ from .dev import *
 SECRET_KEY = 'ci/cd'
 AUTH_PASSWORD_VALIDATORS = []
 PASSWORD_HASHERS = ['django.contrib.auth.hashers.MD5PasswordHasher']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'github_actions',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        }
+    }
