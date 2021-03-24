a691299b94103725d7addf041ee2a5389ad0ab0a
Klement Omeri <klementomeri97@gmail.com>
2021-03-24 22:47:43 +0100

    chore: edit leather images to webp and dump to yaml file

--- italgold/urls.py
+++ italgold/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('order.urls')),
 
     prefix_default_language=False
-    )
+    ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = "ITALGOLD Admin"
 admin.site.site_title = "ITALGOLD Admin Portal"
