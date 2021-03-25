6c177cc299ff39923d1d2ac7fdc96e82cb709210
Klement Omeri <klementomeri97@gmail.com>
2021-03-26 00:52:45 +0100

    chore: edit static storage settings

--- italgold/aws.py
+++ italgold/aws.py
@@ -3,7 +3,6 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     bucket_name = 'italgoldstaticassets'
-    signature_version = 's3v4'
-    querystring_auth = True
+    querystring_auth = False
     gzip = True
     default_acl = 'public_read'
