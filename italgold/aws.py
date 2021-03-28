72560fe729b698e139ae5d04a50fcab3169f5d91
Klement Omeri <klementomeri97@gmail.com>
2021-03-28 20:07:02 +0200

    chore: edit aws settings

--- italgold/aws.py
+++ italgold/aws.py
@@ -2,7 +2,8 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    bucket_name = 'italgoldstaticassets'
-    querystring_auth = False
+    bucket_name = 'italgold'
+    location = 'static'
+    querystring_auth = True
     gzip = True
     default_acl = 'public_read'
